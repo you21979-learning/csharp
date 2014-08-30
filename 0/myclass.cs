b31a05422152ffc7a1fc8def5a4057451f7af470
you21979 <you2197901@gmail.com>
2014-08-30 14:00:04 +0900

    add first commit

---
+++ 0/myclass.cs
@@ -0,0 +1,19 @@
+using System;
+namespace MyNamespace
+{
+  // define class
+  public class MyClass
+  {
+    public MyClass ()
+    {
+      Console.WriteLine("ctor called");
+    }
+    ~MyClass ()
+    {
+      Console.WriteLine("dtor called");
+    }
+    public void MyMethod ()
+    {
+    }
+  }
+}
